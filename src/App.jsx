7d1b482c96d6eb623896f6d9400aec59f7c7ad80
Sanjaee <afrizaahmad18@gmail.com>
2023-12-02 20:16:46 +0700

    revisi5

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,75 @@
 import React, { useState } from "react";
-import { downloadInstagramVideo } from "./assets/api";
+import axios from "axios";
 
-function App() {
-  const [videoUrl, setVideoUrl] = useState("");
-  const [videoData, setVideoData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+const InstagramDownloader = () => {
+  const [result, setResult] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleDownload = async () => {
-    if (videoUrl) {
-      setIsLoading(true);
-      try {
-        const data = await downloadInstagramVideo(videoUrl);
-        setVideoData(data);
-      } catch (error) {
-        console.error(error);
-      }
-      setIsLoading(false);
+  const fetchData = async (url) => {
+    setLoading(true);
+    const options = {
+      method: "GET",
+      url: "https://instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com/",
+      params: {
+        url: url,
+      },
+      headers: {
+        "X-RapidAPI-Key": "7cb05360f8mshca6918f8ea33103p1c4264jsn97b178de78f8",
+        "X-RapidAPI-Host":
+          "instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setResult(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  return (
-    <div className="container">
-      <video className="background-video" loop muted autoPlay>
-        <source src="https://res.cloudinary.com/deafnuhyi/video/upload/v1691920847/Untitled_cjlqzp.mov" type="video/mp4" />
-      </video>
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setInputValue(inputValue);
+  };
 
-      <div className="content">
-        <h1 className="heading">Instagram Reel Video Downloader</h1>
-        <input type="text" className="input" placeholder="Reel Url" value={videoUrl} onChange={(e) => setVideoUrl(e.target.value)} />
-        {isLoading ? (
-          <div>
-            <div className="loader"></div>
+  return (
+    <div className="instagram-downloader-container">
+      <label htmlFor="instagramUrl">Instagram URL:</label>
+      <input
+        type="text"
+        id="instagramUrl"
+        placeholder="https://www.instagram.com/p/CzJ4dY9vnZw/?utm_source=ig_web_copy_link"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+
+      <button onClick={() => fetchData(inputValue)}>Generate</button>
+
+      {loading && <p>Loading...</p>}
+
+      <div className="result-container">
+        {result.map((url, index) => (
+          <div key={index} className="result-item">
+            <p>Hasil: </p>
+            <p>{index}:</p>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              <button
+                className="download-button"
+                onClick={() => (window.location.href = url)}
+                download
+              >
+                Download
+              </button>
+            </a>
           </div>
-        ) : (
-          videoData && (
-            <div className="video-section">
-              <video className="video-player" controls>
-                <source src={videoData.media} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            </div>
-          )
-        )}
-        <p className="P">KLIK PADA TOMBOL TITIK TIGA LALU CLICK DOWNLOAD</p>
-        <button className="button" onClick={handleDownload}>
-          <span>Download</span>
-        </button>
-        <span className="font-bold text-white mt-4">My Contact</span>
-        <div className="flex justify-center items-center mt-4">
-          <a className="" href="https://www.instagram.com/ahmdfrizza/?hl=en">
-            <img className="w-12 h-12 text-gray-500" src="/img/instagram.png" alt="" />
-          </a>
-          <a className="ml-8" href="https://www.linkedin.com/in/ahmad-afriza-ez4-ab9173276/">
-            <img className="w-12 h-12 text-gray-500" src="/img/linkedin.png" alt="" />
-          </a>
-        </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default InstagramDownloader;
